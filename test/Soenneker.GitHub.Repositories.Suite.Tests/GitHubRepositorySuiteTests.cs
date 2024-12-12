80151ba031217557c036aa2830421214e6b2e90c
Jake Soenneker <jake@soenneker.com>
2024-12-12 11:58:59 -0600

    Fixes build

--- test/Soenneker.GitHub.Repositories.Suite.Tests/GitHubRepositorySuiteTests.cs
+++ test/Soenneker.GitHub.Repositories.Suite.Tests/GitHubRepositorySuiteTests.cs
@@ -1,6 +1,3 @@
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
-using Soenneker.GitHub.Repositories.Suite.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
 using Xunit.Abstractions;
@@ -10,10 +7,7 @@ namespace Soenneker.GitHub.Repositories.Suite.Tests;
 [Collection("Collection")]
 public class GitHubRepositorySuiteTests : FixturedUnitTest
 {
-    private readonly IGitHubRepositorySuite _util;
-
     public GitHubRepositorySuiteTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
     {
-        _util = Resolve<IGitHubRepositorySuite>(true);
     }
 }
